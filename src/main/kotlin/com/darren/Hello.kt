5c2f1db59688e845e88c1244748697a6987091c5
Darren Huang <darren.jl.huang@mail.foxconn.com>
2019-03-24 20:17:22 +0800

    Initial commit

---
+++ src/main/kotlin/com/darren/Hello.kt
@@ -0,0 +1,13 @@
+package com.darren
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+    val h = Human()
+    h.Hello()
+}
+
+class Human {
+    fun Hello() {
+        println("Hello Kotlin")
+    }
+}
